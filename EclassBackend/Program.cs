8118cc9f55a4a9e29f47a8ef12fc3d8cd8498548
apost <apost@DESKTOP-77GJSMF>
2022-10-25 23:00:59 +0300

    s

--- EclassBackend/Program.cs
+++ EclassBackend/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<EclassContext>(opt =>
-    opt.UseInMemoryDatabase("Eclass"));
+    opt.UseInMemoryDatabase("EclassDB"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
